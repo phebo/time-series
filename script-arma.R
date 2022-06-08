219535d99cffbb5ebdc250c56e3c584a1cd5a5f0
Phebo Wibbens <phebo.wibbens@insead.edu>
2022-06-08 11:28:57 +0200

    Add in-class ARMA code

--- script-arma.R
+++ script-arma.R
@@ -1,18 +1,25 @@
 library(tidyverse)
 
+#### MA(1) ####
+
+xt <- arima.sim(list(ma=-0.3), 100)
+plot(xt)
+acf(xt)
+
 
 #### ARMA(1,1) ####
 lam <- 0.9
 seps <- 1
-sdelta <- 0.5
+sdelta <- 2
 n <- 1e5
 
-
 xt <- arima.sim(list(ar = lam), n) * seps
+plot(xt)
 var(xt)
 seps^2 / (1-lam^2)
 
 yt <- xt + rnorm(n, sd = sdelta)
+plot(yt)
 fit1 <- arima(yt, order = c(1,0,0), include.mean = F)
 fit2 <- arima(yt, order = c(1,0,1), include.mean = F)
 fit1
@@ -34,9 +41,38 @@ sim <- function(lam, n) {
 dfSim <- map_dfr(1:100, ~ sim(lam = 0.9, n = 50))
 
 dfLam <- dfSim %>% filter(par == "lam") %>% arrange(est) %>%
-  mutate(i = row_number(), ymin = est - 1.96 * se, ymax = est + 1.96 * se, )
-ggplot(dfLam, aes(x = i, y = est, ymin=ymin, ymax=ymax)) + geom_pointrange()
+  mutate(i = row_number(), ymin = est - 1.96 * se, ymax = est + 1.96 * se)
+ggplot(dfLam, aes(x = i, y = est, ymin=ymin, ymax=ymax)) + geom_pointrange() +
+  geom_hline(yintercept = 0.9)
 
+# ARMA(1,1) regression
+sim <- function(lam, sdelta, n) {
+  xt <- arima.sim(list(ar = lam), n)
+  yt <- xt + rnorm(n, sd = sdelta)
+  reg.data <- tibble(y=as.vector(yt), x=lag(y))
+  fit <- lm(y ~ x, reg.data)
+  tibble(par = c("cst", "lam"), est = coef(fit), se = sqrt(diag(vcov(fit))))
+}
+dfSim <- map_dfr(1:100, ~ sim(lam = 0.9, sdelta = 1, n = 1e4))
+
+dfLam <- dfSim %>% filter(par == "lam") %>% arrange(est) %>%
+  mutate(i = row_number(), ymin = est - 1.96 * se, ymax = est + 1.96 * se)
+ggplot(dfLam, aes(x = i, y = est, ymin=ymin, ymax=ymax)) + geom_pointrange() +
+  geom_hline(yintercept = lam)
+
+# ARMA(1,1) ML estimation
+sim <- function(lam, sdelta, n) {
+  xt <- arima.sim(list(ar = lam), n)
+  yt <- xt + rnorm(n, sd = sdelta)
+  fit <- arima(yt, order = c(1,0,1))
+  tibble(par = c("lam", "beta", "cst"), est = coef(fit), se = sqrt(diag(vcov(fit))))
+}
+dfSim <- map_dfr(1:100, ~ sim(lam = 0.95, sdelta = 0.2, n = 200))
+
+dfLam <- dfSim %>% filter(par == "lam") %>% arrange(est) %>%
+  mutate(i = row_number(), ymin = est - 1.96 * se, ymax = est + 1.96 * se)
+ggplot(dfLam, aes(x = i, y = est, ymin=ymin, ymax=ymax)) + geom_pointrange() +
+  geom_hline(yintercept = lam)
 
 #### AR(2) ####
 
